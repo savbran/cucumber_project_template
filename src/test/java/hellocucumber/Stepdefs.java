460f25632e6fe6c09ccd5e1d7dd07b9d25f44c9b
Saverio Brancaccio <saverio.brancaccio@gmail.com>
2018-12-14 12:32:44 +0100

    Project updated

--- src/test/java/hellocucumber/Stepdefs.java
+++ src/test/java/hellocucumber/Stepdefs.java
@@ -1,26 +1,66 @@
 package hellocucumber;
 
+import static org.junit.Assert.*;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import cucumber.api.java.en.Then;
-import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Stepdefs {
-	@Given("^The web browser is opened on google hompage$")
+
+	private WebDriver browser;
+
+	@Given("^The web browser is opened on google home page$")
 	public void startBrowserWithGooglePage() throws Exception {
-		// Write code here that turns the phrase above into concrete actions
-		assertTrue(true);
+		// Get the browser
+		WebDriverManager.chromedriver().setup();
+		browser = new ChromeDriver();
+
+		// Set the browser home page
+		browser.get("http://www.google.com");
+
+		// Verify home page
+		String pageTitle = browser.getTitle();
+		Boolean pageTitleIsOk = pageTitle.equals("Google");
+		assertTrue(pageTitleIsOk);
+
 	}
 
 	@When("^The user inserts a text sentence in search input element$")
-	public void setGoogleSearchText() throws Exception {
-		// Write code here that turns the phrase above into concrete actions
-		assertTrue(true);
+	public void searchTextInGoogle() throws Exception {
+		// search a text sentence in google search input field
+		WebElement searchField = browser.findElement(By.name("q"));
+		searchField.sendKeys("Price of lord of the rings book");
+		searchField.sendKeys(Keys.ENTER);
+		TimeUnit.SECONDS.sleep(3);
+
 	}
 
 	@Then("^The google homepage shows searching results$")
 	public void verifyGoogleSearchResults() throws Exception {
-		// Write code here that turns the phrase above into concrete actions
-		assertTrue(true);
+		// wait until the google page shows the result
+
+		List<WebElement> findElements = browser.findElements(By.xpath("//*[@id='rso']//h3/a"));
+
+		// this are all the links you like to visit
+		for (WebElement webElement : findElements) {
+			System.out.println(webElement.getAttribute("href"));
+		}
+
+		// Quit the browser
+		TimeUnit.SECONDS.sleep(5);
+		if (browser != null)
+			browser.quit();
 	}
+
 }
